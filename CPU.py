3e12a4ac7c0e4788b6f62bacfda72ab348e18961
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-03-13 22:10:04 -0600

    visual things fixed.

--- CPU.py
+++ CPU.py
@@ -22,12 +22,12 @@ def process (name, env, memory, cpu, arrival, instructionsq, ramq): # Elementos 
     # Graba el tiempo de llegada.
     arrivalTime = env.now
 
-    print('%s Proceso en cola [NEW llegada] -> %d cantidad ram requerida %d, disponible %d' % (name, env.now, ramq, memory.level))
+    print('%s \nProceso en cola [NEW llegada] -> %d cantidad ram requerida %d, disponible %d' % (name, env.now, ramq, memory.level))
     yield memory.get(ramq)  # Solicita la memoria que necesita o espera automaticamente hasta que haya suficiente.
 
     while instructionsq > 0:  # Loop hasta que se acabe la cantidad de instrucciones pendientes.
         # Memoria inicial disponible
-        print('%s proceso en cola READY tiempo -> %d cantidad instrucciones pendientes %d' % (name, env.now, instructionsq))
+        print('%s \nProceso en cola READY tiempo -> %d Cantidad instrucciones pendientes %d' % (name, env.now, instructionsq))
 
         with cpu.request() as req:  # Solicitud al procesador.
             yield req
@@ -36,14 +36,14 @@ def process (name, env, memory, cpu, arrival, instructionsq, ramq): # Elementos 
             yield env.timeout(1) # Simulacion de un ciclo de reloj del procesador.
 
             # Existencia de un procesador.
-            print('%s proceso en estado RUNNING fue atendido en tiempo -> %d cantidad ram %d, Instrucciones pendientes %d ram disponible %d' % (name, env.now, ramq, instructionsq, memory.level))
+            print('%s \nProceso en estado RUNNING fue atendido en tiempo -> %d Cantidad ram %d, Instrucciones pendientes %d ram disponible %d' % (name, env.now, ramq, instructionsq, memory.level))
 
     # Retorna la memoria utilizada.
     yield memory.put(ramq)
-    print('%s proceso TERMINATED salida -> %d cantidad ram devuelta %d, nueva cantidad de memoria disponible %d' % (name, env.now, ramq, memory.level))
+    print('%s \nProceso TERMINATED salida -> %d cantidad ram devuelta %d, nueva cantidad de memoria disponible %d' % (name, env.now, ramq, memory.level))
     global totalTime
     totalTime += env.now - arrivalTime
-    print('Tiempo total %d' % (env.now - arrivalTime)) # Tiempo total en base al arrivalTime.
+    print('\nTiempo total %d' % (env.now - arrivalTime)) # Tiempo total en base al arrivalTime.
 
 # --- Implementacion de la semilla ---
 random.seed(RandomSeed)
@@ -58,10 +58,10 @@ for i in range(initialProcess):
     arrival = 0 # Todos los procesos llegan al mismo tiempo.
     instructionsq = random.randint(1, 10)  # Cantidad de operaciones por proceso.
     ramUse = random.randint(1, 10)  # Cantidad de ram que requiere cada proceso.
-    env.process(process('proceso %d' % i, env, initialRam, initialCpu, arrival, instructionsq, ramUse))
+    env.process(process('proceso %d' % i, env, initialRam, initialCpu, arrival, instructionsq, ramUse)) # Parametros.
 
     # Se corre la simulacion.
     env.run()
-    print('Tiempo promedio %d ' % (totalTime / initialProcess))
+    print('\nTiempo promedio %d ' % (totalTime / initialProcess))
 
 
